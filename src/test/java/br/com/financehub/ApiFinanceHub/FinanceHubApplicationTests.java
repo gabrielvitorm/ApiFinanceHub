13593d521dc5de51d12d619efac4b9cca5a453b5
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2025-03-29 18:20:55 -0300

    Subindo arquivos do Projeto

---
+++ src/test/java/br/com/financehub/ApiFinanceHub/FinanceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.financehub.ApiFinanceHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
