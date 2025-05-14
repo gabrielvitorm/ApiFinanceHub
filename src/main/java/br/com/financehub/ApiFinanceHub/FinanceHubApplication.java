fccd6809711beeafd40f18893cf3c3befd6471e8
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2025-05-14 14:31:46 -0300

    Liberação dos endpoints para testes

--- src/main/java/br/com/financehub/ApiFinanceHub/FinanceHubApplication.java
+++ src/main/java/br/com/financehub/ApiFinanceHub/FinanceHubApplication.java
@@ -2,8 +2,13 @@ package br.com.financehub.ApiFinanceHub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		SecurityAutoConfiguration.class,
+		ManagementWebSecurityAutoConfiguration.class
+})
 public class FinanceHubApplication {
 
 	public static void main(String[] args) {
